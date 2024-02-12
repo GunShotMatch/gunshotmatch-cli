fac717dadf4ec3e0c80605f57982918642f54e20
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2024-02-12 15:15:27 +0000

    Bump version v0.5.1 -> v0.6.0

--- gunshotmatch_cli/__init__.py
+++ gunshotmatch_cli/__init__.py
@@ -29,5 +29,5 @@ GunShotMatch Command-Line Interface.
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2020-2023 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.5.1"
+__version__: str = "0.6.0"
 __email__: str = "dominic@davis-foster.co.uk"
